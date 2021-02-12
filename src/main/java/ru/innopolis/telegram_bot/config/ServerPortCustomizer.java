8c8f02496256f94da9969d61212913cb5eeb1b4e
Danila Kochan <d.kochan@innopolis.ru>
2021-02-12 20:42:57 +0300

    add port settings

---
+++ src/main/java/ru/innopolis/telegram_bot/config/ServerPortCustomizer.java
@@ -0,0 +1,15 @@
+package ru.innopolis.telegram_bot.config;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServerPortCustomizer
+        implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        factory.setPort(Integer.parseInt(System.getenv("PORT")));
+    }
+}
